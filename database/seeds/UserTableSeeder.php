4b6a691dc20c7e1f7cc427e5bdbf71d481519b3f
Areeba <areebanovember@hotmail.com>
2020-05-01 16:35:16 +0500

    seederupdated

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,18 +13,55 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Factory::create();
-
-        User::truncate();
-
-        foreach(range(1, 10) as $i) {
-            User::create([
-                'name' => $faker->name,
-                'email' => $faker->unique()->email,
-                'email_verified_at' => now(),
-                'password' => bcrypt('password'),
-                'remember_token' => Str::random(10),
-            ]);
-        }
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "areebanovember@hotmail.com",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "ekanhoi.makh@osatna.ga",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "mohamed.roch@dwsfhe.us",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "skeiven.santos.78@textcasi.ml",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "fbgakhary@livingshoot.com",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "9caporal.cavid.a0@cek-resi.website",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "clucase19982h@greendipbench.ga",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "frooney.single.7@textcasi.ml",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "wbenja@theblogprofboe.info",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
+        DB::table('users')->insert([
+            'name' => "user1",
+            'email' => "9moises.santan@volthdas.gq",
+            'password' => "$2y$10$8.TGzjXVU0zTT.jUKTAkpuqsLXoCkvpc73qFKEhfSbr.U/9cUGH6O",
+        ]);
     }
 }
