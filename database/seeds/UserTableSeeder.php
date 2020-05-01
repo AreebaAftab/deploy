f370a7d20697e9803b3eabc305b9a0c790a3abfc
Areeba <areebanovember@hotmail.com>
2020-05-01 16:14:09 +0500

    seederupdate

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\User;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        User::truncate();
+
+        foreach(range(1, 10) as $i) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->email,
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+    }
+}
